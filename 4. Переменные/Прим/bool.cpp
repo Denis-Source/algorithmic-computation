b046450113b312a0708bd58b477e2f7420a69ff3
Denis-Source <zolotoverkhdenis@gmail.com>
2022-02-14 10:01:06 +0200

    4 lecture

---
+++ 4. Переменные/Прим/bool.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout << isCodingFun;  // Выводит 1 (true)
+    cout << isFishTasty;  // Выводит 0 (false)
+}
---
+++ 4. Переменные/Прим/float.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // creating float type variables
+    float num1 = 3.0f;
+    float num2 = 3.5f;
+    float num3 = 3E-5f; // 3x10^-5
+
+    // creating double type variables
+    double num4 = 3.0;
+    double num5 = 3.5;
+    double num6 = 3E-5; // 3x10^-5
+}
